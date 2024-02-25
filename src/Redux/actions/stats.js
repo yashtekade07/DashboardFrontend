a537650ebcec6886fb31c4ddc6db3da582101828
Deceit <yashtekade07@gmail.com>
2024-02-26 02:10:53 +0530

    host

--- src/Redux/actions/stats.js
+++ src/Redux/actions/stats.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getCallEntries =
   ({ campaignId, startDate, endDate }) =>
   async (dispatch) => {
     try {
-      dispatch({ type: "getCallEntryRequest" });
+      dispatch({ type: 'getCallEntryRequest' });
       const { data } = await axios.get(
-        `http://localhost:5000/api/callentries?campaignId=${
-          campaignId || ""
-        }&startDate=${startDate || ""}&endDate=${endDate || ""}`
+        `https://dashboardbackend-dq4m.onrender.com/callentries?campaignId=${
+          campaignId || ''
+        }&startDate=${startDate || ''}&endDate=${endDate || ''}`
       );
-      dispatch({ type: "getCallEntrySuccess", payload: data });
+      dispatch({ type: 'getCallEntrySuccess', payload: data });
     } catch (error) {
       dispatch({
-        type: "getCallEntryFail",
+        type: 'getCallEntryFail',
         payload: error.response.data.message,
       });
     }
